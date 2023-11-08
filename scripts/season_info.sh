2686a32b1a6267008ae3fe54eb3212df2e38f3a4
martinAleB <martbarr@gmail.com>
2023-11-08 20:16:56 -0300

    feat: add remove namespaces

--- scripts/season_info.sh
+++ scripts/season_info.sh
@@ -1,2 +1,3 @@
 mkdir -p data
 curl "http://api.sportradar.us/rugby-league/trial/v3/en/seasons/$2/info.xml?api_key=${API_KEY}" -o ./data/$1
+java net.sf.saxon.Transform -s:data/$1 -xsl:helpers/remove_namespaces.xsl -o:data/$1
