041331acbf40925ea349a46fc75e578b2edf91f4
martinAleB <martbarr@gmail.com>
2023-11-08 23:38:41 -0300

    feat: add main script

--- tpe.sh
+++ tpe.sh
@@ -1,7 +1,5 @@
 if [ $# -ne 2 ]
 then
-    # TIENE QUE DEVOLVERLO EL MARKDOWN
-    echo "Error: two arguments (name_prefix, year) were expected, but $# were received."
     exit 1
 fi
 
@@ -16,6 +14,18 @@ year=$2
 ./scripts/seasons.sh $SEASONS_FILE
 
 # Get season id for name_prefix and year received
-java net.sf.saxon.Query "seasons_file=$SEASONS_FILE" "season_prefix=$name_prefix" "season_year=$year" ./queries/extract_season_id.xq
+season_id=$(java net.sf.saxon.Query "seasons_file=$SEASONS_FILE" "season_prefix=$name_prefix" "season_year=$year" ./queries/extract_season_id.xq)
 
+# Call to information and lineups endpoint
+./scripts/season_info.sh $SEASON_INFO_FILE $season_id
+./scripts/season_lineups.sh $SEASON_LINEUPS_FILE $season_id
 
+echo $?
+
+### COSAS QUE FALTAN
+#   - Intercomunicacion con xq que genera xml con la data para pasarle estados de error previos.
+#   - En caso de que la informacion sea correcta, validar contra el xsd el xml generado.
+
+### ERRORES POSIBLES HASTA ACA
+#   - Cannot convert string "a" to an integer (no entero en $2)
+#   - Id no encontrado !!! NO HANDLEA, guarda EMPTY en season_id
